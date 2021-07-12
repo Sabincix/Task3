61720c18af98f08a84df6e27f8952ac832cbd0d3
Sabincix <kastaljana@gmail.com>
2021-07-12 21:29:51 +0300

    Initial commit

---
+++ src/Task3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        short day;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a day");
+        day = scanner.nextShort();
+
+        System.out.println("The day number is: " + day );
+
+        if (day < 0) {
+            System.out.println(" Day can't be negative");
+        } else if ( day > 365) {
+            System.out.println("Dat can't be that large");
+        } else {
+           short month = (short) (Math.ceil(day / 30) + 1);
+           switch (month) {
+               case 1:
+                   System.out.println("It is January");
+                   break;
+               case 2:
+                   System.out.println("It is February");
+                   break;
+
+
+           }
+       }
+    }
+}
